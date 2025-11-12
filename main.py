4f8b3643cf803e8af11032c22e0e12ce0d70c24c
Manu Mohan <manu.mohan0303@gmail.com>
2025-11-12 23:18:16 +0100

    Added First set of API Calls

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import requests
+
+def checking_aviationstack():
+    params = {
+    'access_key': '406a10e437a689315b4e6b436096e78b'
+    }
+
+    api_result = requests.get('https://api.aviationstack.com/v1/flights', params)
+
+    api_response = api_result.json()
+
+    for flight in api_response['data']:
+        print(u'%s flight %s from %s (%s) to %s (%s) is in the air.' % (
+            flight['airline']['name'],
+            flight['flight']['iata'],
+            flight['departure']['airport'],
+            flight['departure']['iata'],
+            flight['arrival']['airport'],
+            flight['arrival']['iata']))
+
 
 def main():
-    pass
+    checking_aviationstack()
 
 if __name__ == "__main__":
     main()
