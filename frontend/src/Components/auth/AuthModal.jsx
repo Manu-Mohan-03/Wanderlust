b2f7d42abf613ae5a004901563559921a5190365
Manu-Mohan-03 <manu.mohan0303@gmail.com>
2026-03-31 21:59:08 +0200

    Modal Popup text not visible - Fixed

--- frontend/src/Components/auth/AuthModal.jsx
+++ frontend/src/Components/auth/AuthModal.jsx
@@ -5,7 +5,7 @@ export default function AuthModal({ onClose }) {
     <div className='backdrop' onClick={onClose}>  
         {/* Modal — stop click propagating to backdrop, so that clicking any where on modal donot close it */}
         <div className='modal' onClick={e => e.stopPropagation()}>
-            <h2>Sign In / Create Account</h2>
+            <h2 style={{color: 'black'}}>Sign In / Create Account</h2>
         </div>
     </div>
   )
