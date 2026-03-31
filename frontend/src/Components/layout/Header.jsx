20651286be46527b422655ab3da732565f53381e
Manu-Mohan-03 <manu.mohan0303@gmail.com>
2026-03-31 19:07:38 +0200

    Sign In Button Created in Header

--- frontend/src/Components/layout/Header.jsx
+++ frontend/src/Components/layout/Header.jsx
@@ -7,6 +7,12 @@ export default function Header() {
                 <div className='logo'>
                 ✈️ <span className='logo-text'>WanderLust</span>
                 </div>
+                {/* Right — Login Button*/}
+                <div className='right'>
+                    <button className="btn">
+                        Sign In
+                    </button>
+                </div>
             </header>
        </>
     )
